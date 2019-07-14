2a30b73e36ae4d7560acb8f4124e0012577f1421
theashishmalik <imashishmalik.45@gmail.com>
2019-07-14 23:17:36 +0530

    slot concept

---
+++ Component-Adv/cmp-adv/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <app-quote>
+          <h1>{{quoteHead}}</h1>
+          <p>A beautiful quote!</p>
+      </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+export default {
+    data(){
+        return{
+            quoteHead:"The Quote"
+        }
+    },
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Component-Adv/cmp-adv/src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+       <slot>
+       
+   </slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div{
+    background-color: cadetblue;
+    box-shadow: 1px 1px 5px black;
+    margin: 20px auto;
+    padding: 30px;
+    text-align: center;
+    color: red;
+    font-size: 14pt;
+}
+h1{
+    color: black;
+    text-emphasis: 4px;
+}
+
+</style>
