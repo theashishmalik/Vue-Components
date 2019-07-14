c33e815244b3ee215d846cc10f08c260398cdb34
theashishmalik <imashishmalik.45@gmail.com>
2019-07-15 00:25:55 +0530

    added dynamic components

--- Component-Adv/cmp-adv/src/App.vue
+++ Component-Adv/cmp-adv/src/App.vue
@@ -1,22 +1,46 @@
 <template>
-  <div>
+  <section>
+    <div>
+      <button @click="selected='appBlue'">Blue</button>
+      <button @click="selected='appGreen'">Green</button>
+      <button @click="selected='appRed'">Red</button>
+      <hr />
+      <p>{{selected}}</p>
+      <hr />
+      <div>
+        <component :is="selected">
+          <p>Here it is</p>
+        </component>
+      </div>
+    </div>
+    <hr />
+    <hr />
+    <div>
       <app-quote>
-          <h1>{{quoteHead}}</h1>
-          <p>A beautiful quote!</p>
+        <h1 slot="title">{{quoteHead}}</h1>
+        <p slot="quote">A beautiful quote!</p>
       </app-quote>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import Quote from "./components/Quote.vue";
+import Red from "./components/Red.vue";
+import Green from "./components/Green";
+import Blue from "./components/Blue.vue";
 export default {
-    data(){
-        return{
-            quoteHead:"The Quote"
-        }
-    },
+  data() {
+    return {
+      quoteHead: "The Quote",
+      selected: "appRed"
+    };
+  },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appRed: Red,
+    appBlue: Blue,
+    appGreen: Green
   }
 };
 </script>
--- Component-Adv/cmp-adv/src/components/Quote.vue
+++ Component-Adv/cmp-adv/src/components/Quote.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
-       <slot>
-       
-   </slot>
+    <div>
+      <slot name="title"></slot>
+    </div>
+    <hr>
+    <div>
+        <slot name="quote"></slot>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-div{
-    background-color: cadetblue;
-    box-shadow: 1px 1px 5px black;
-    margin: 20px auto;
-    padding: 30px;
-    text-align: center;
-    color: red;
-    font-size: 14pt;
+div {
+  background-color: cadetblue;
+  box-shadow: 1px 1px 5px black;
+  margin: 20px auto;
+  padding: 30px;
+  text-align: center;
+  color: red;
+  font-size: 14pt;
 }
-h1{
-    color: black;
-    text-emphasis: 4px;
+h1 {
+  color: black;
+  text-emphasis: 4px;
 }
-
 </style>
